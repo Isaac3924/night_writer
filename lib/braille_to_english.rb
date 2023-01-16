75152924087283c693b74e85009a91d1d3ff5979
Isaac Alter <i_alter@yahoo.com>
2023-01-16 14:01:40 -0700

    Adds refactor: spacing set to 2

--- lib/braille_to_english.rb
+++ lib/braille_to_english.rb
@@ -1,85 +1,60 @@
 class BrailleToEnglish
-    attr_reader :message
+  attr_reader :message
   
-    def initialize(message)
-      @message = message
-    end
-  
-    def translate
-      braille_alhpabet = { "a" => ["0.", "..", ".."],
-                           "b" => ["0.", "0.", ".."],
-                           "c" => ["00", "..", ".."],
-                           "d" => ["00", ".0", ".."],
-                           "e" => ["0.", ".0", ".."],
-                           "f" => ["00", "0.", ".."],
-                           "g" => ["00", "00", ".."],
-                           "h" => ["0.", "00", ".."],
-                           "i" => [".0", "0.", ".."],
-                           "j" => [".0", "00", ".."],
-                           "k" => ["0.", "..", "0."],
-                           "l" => ["0.", "0.", "0."],
-                           "m" => ["00", "..", "0."],
-                           "n" => ["00", ".0", "0."],
-                           "o" => ["0.", ".0", "0."],
-                           "p" => ["00", "0.", "0."],
-                           "q" => ["00", "00", "0."],
-                           "r" => ["0.", "00", "0."],
-                           "s" => [".0", "0.", "0."],
-                           "t" => [".0", "00", "0."],
-                           "u" => ["0.", "..", "00"],
-                           "v" => ["0.", "0.", "00"],
-                           "w" => [".0", "00", ".0"],
-                           "x" => ["00", "..", "00"],
-                           "y" => ["00", ".0", "00"],
-                           "z" => ["0.", ".0", "00"],
-                           " " => ["..", "..", ".."]
-                          }
-  
-      braille_message = []
-      message_slots = []
-  
-      @message.chars.each do |letter|
-        match = braille_alhpabet.keys.find {|key| key == letter}
-        if match == nil
-          braille_message << letter
-        else
-          braille_message << braille_alhpabet[match]
-        end
-      end
-  
-      braille_format = Hash.new{ |k, v| k[v] = [] }
-  
-      braille_message.each do |braille_letter|
-        braille_format[1] << braille_letter[0]
-        braille_format[2] << braille_letter[1]
-        braille_format[3] << braille_letter[2]
-      end
-  
-      braille_format_actual = Hash.new{ |k, v| k[v] = [] }
-      storage = []
-  
-      braille_format.each do |line, row_array|
-        i = line
-        if row_array.count > 40
-          storage = braille_format[line].pop(row_array.count - 40)
-          braille_format_actual[i] = row_array
-          while storage.count > 0
-            i += 3
-            braille_format_actual[i] = storage.shift(40)
-          end
-        else
-          braille_format_actual[i] = row_array
-        end
-      end
-  
-      braille_format_actual = braille_format_actual.sort_by{|k| k}.to_h
-  
-      final_message_array = []
-  
-      braille_format_actual.each do |line, row_array|
-        final_message_array << "#{row_array.join}\n"
-      end
-  
-      final_message_array.join
-    end
+  def initialize(message)
+    @message = message
   end
+  
+  def translate
+    braille_alhpabet = { "z" => ["0.", ".0", "00"],
+                          " " => ["..", "..", ".."]
+                        }
+  
+    braille_message = []
+    message_slots = []
+  
+    @message.chars.each do |letter|
+      match = braille_alhpabet.keys.find {|key| key == letter}
+      if match == nil
+        braille_message << letter
+      else
+        braille_message << braille_alhpabet[match]
+      end
+    end
+  
+    braille_format = Hash.new{ |k, v| k[v] = [] }
+  
+    braille_message.each do |braille_letter|
+      braille_format[1] << braille_letter[0]
+      braille_format[2] << braille_letter[1]
+      braille_format[3] << braille_letter[2]
+    end
+  
+    braille_format_actual = Hash.new{ |k, v| k[v] = [] }
+    storage = []
+  
+    braille_format.each do |line, row_array|
+      i = line
+      if row_array.count > 40
+        storage = braille_format[line].pop(row_array.count - 40)
+        braille_format_actual[i] = row_array
+        while storage.count > 0
+          i += 3
+          braille_format_actual[i] = storage.shift(40)
+        end
+      else
+        braille_format_actual[i] = row_array
+      end
+    end
+  
+    braille_format_actual = braille_format_actual.sort_by{|k| k}.to_h
+  
+    final_message_array = []
+  
+    braille_format_actual.each do |line, row_array|
+      final_message_array << "#{row_array.join}\n"
+    end
+  
+      inal_message_array.join
+  end
+end
--- lib/night_reader_class.rb
+++ lib/night_reader_class.rb
@@ -2,35 +2,35 @@ require_relative './braille_to_english'
 
 class NightReaderClass
   
-    attr_reader :handle,
-                :incoming_text,
-                :new_file_characters,
-                :outgoing_text
+  attr_reader :handle,
+              :incoming_text,
+              :new_file_characters,
+              :outgoing_text
     
-    def initialize(file_name1)
-      @handle = File.open(file_name1, "r")
-      @incoming_text = @handle.read
-      @new_file_characters = 0
-      @outgoing_text = ''
-    end
+  def initialize(file_name1)
+    @handle = File.open(file_name1, "r")
+    @incoming_text = @handle.read
+    @new_file_characters = 0
+    @outgoing_text = ''
+  end
     
-    def self.from_terminal(file_name1)
-      if File.exists?(file_name1)
-        NightReaderClass.new(file_name1)
-      else
-        puts "The first file is not in the directory. Please input a file that is."
-      end
-    end
-    
-    def change_text
-      translated_text = BrailleToEnglish.new(@incoming_text)
-      @outgoing_text = translated_text.translate
-    end
-    
-    def make_file(file_name2)
-      writer = File.open(file_name2, "w")
-      writer.write(@outgoing_text)
-  
-      puts "Created '#{File.basename(writer)}' containing #{new_file_characters} characters, from '#{File.basename(@handle)}'."
+  def self.from_terminal(file_name1)
+    if File.exists?(file_name1)
+      NightReaderClass.new(file_name1)
+    else
+      puts "The first file is not in the directory. Please input a file that is."
     end
   end
+    
+  def change_text
+    translated_text = BrailleToEnglish.new(@incoming_text)
+    @outgoing_text = translated_text.translate
+  end
+    
+  def make_file(file_name2)
+    writer = File.open(file_name2, "w")
+    writer.write(@outgoing_text)
+  
+    puts "Created '#{File.basename(writer)}' containing #{new_file_characters} characters, from '#{File.basename(@handle)}'."
+  end
+end

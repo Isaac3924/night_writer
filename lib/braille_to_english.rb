fc6d79e611f2af5b60c2bf9239245ecb4fc936ef
Isaac Alter <i_alter@yahoo.com>
2023-01-16 14:33:28 -0700

    Adds feat: full Braille to English alphabet hash

--- lib/braille_to_english.rb
+++ lib/braille_to_english.rb
@@ -6,55 +6,83 @@ class BrailleToEnglish
   end
   
   def translate
-    braille_alhpabet = { "z" => ["0.", ".0", "00"],
-                          " " => ["..", "..", ".."]
-                        }
+    
+    braille_alhpabet = { ["0.", "..", ".."] => "a",
+                         ["0.", "..", ".."] => "b",
+                         ["00", "..", ".."] => "c",
+                         ["00", ".0", ".."] => "d",
+                         ["0.", ".0", ".."] => "e",
+                         ["00", "0.", ".."] => "f",
+                         ["00", "00", ".."] => "g",
+                         ["0.", "00", ".."] => "h",
+                         [".0", "0.", ".."] => "i",
+                         [".0", "00", ".."] => "j",
+                         ["0.", "..", "0."] => "k",
+                         ["0.", "0.", "0."] => "l",
+                         ["00", "..", "0."] => "m",
+                         ["00", ".0", "0."] => "n",
+                         ["0.", ".0", "0."] => "o",
+                         ["00", "0.", "0."] => "p",
+                         ["00", "00", "0."] => "q",
+                         ["0.", "00", "0."] => "r",
+                         [".0", "0.", "0."] => "s",
+                         [".0", "00", "0."] => "t",
+                         ["0.", "..", "00"] => "u",
+                         ["0.", "0.", "00"] => "v",
+                         [".0", "00", ".0"] => "w",
+                         ["00", "..", "00"] => "x",
+                         ["00", ".0", "00"] => "y",
+                         ["0.", ".0", "00"] => "z",
+                         ["..", "..", ".."] => " " 
+  }
   
-    braille_message = []
-    message_slots = []
+  braille_message = []
+  message_slots = []
   
-    @message.chars.each do |letter|
-      match = braille_alhpabet.keys.find {|key| key == letter}
-      if match == nil
-        braille_message << letter
-      else
-        braille_message << braille_alhpabet[match]
+  @message.split.each do |row|
+    require 'pry'; binding.pry
+    match = braille_alhpabet.keys.find {|key| key == row}
+    if match == nil
+      braille_message << row
+    else
+      braille_message << braille_alhpabet[match]
+      require 'pry'; binding.pry
+    end
+  end
+  
+  braille_format = Hash.new{ |k, v| k[v] = [] }
+  
+  braille_message.each do |braille_letter|
+    braille_format[1] << braille_letter[0]
+    braille_format[2] << braille_letter[1]
+    braille_format[3] << braille_letter[2]
+  end
+  
+  braille_format_actual = Hash.new{ |k, v| k[v] = [] }
+  storage = []
+  
+  braille_format.each do |line, row_array|
+    i = line
+    if row_array.count > 40
+      storage = braille_format[line].pop(row_array.count - 40)
+      braille_format_actual[i] = row_array
+      while storage.count > 0
+        i += 3
+        braille_format_actual[i] = storage.shift(40)
       end
+    else
+      braille_format_actual[i] = row_array
     end
+  end
   
-    braille_format = Hash.new{ |k, v| k[v] = [] }
+  braille_format_actual = braille_format_actual.sort_by{|k| k}.to_h
   
-    braille_message.each do |braille_letter|
-      braille_format[1] << braille_letter[0]
-      braille_format[2] << braille_letter[1]
-      braille_format[3] << braille_letter[2]
-    end
+  final_message_array = []
   
-    braille_format_actual = Hash.new{ |k, v| k[v] = [] }
-    storage = []
+  braille_format_actual.each do |line, row_array|
+    final_message_array << "#{row_array.join}\n"
+  end
   
-    braille_format.each do |line, row_array|
-      i = line
-      if row_array.count > 40
-        storage = braille_format[line].pop(row_array.count - 40)
-        braille_format_actual[i] = row_array
-        while storage.count > 0
-          i += 3
-          braille_format_actual[i] = storage.shift(40)
-        end
-      else
-        braille_format_actual[i] = row_array
-      end
-    end
-  
-    braille_format_actual = braille_format_actual.sort_by{|k| k}.to_h
-  
-    final_message_array = []
-  
-    braille_format_actual.each do |line, row_array|
-      final_message_array << "#{row_array.join}\n"
-    end
-  
-      final_message_array.join
+    final_message_array.join
   end
 end

2d14f600ed77998c5d3ff008ecf3ed883488aebd
Isaac Alter <i_alter@yahoo.com>
2023-01-17 13:36:42 -0700

    add refactor: further testing coverage, all_spec still at 100% coverage

--- spec/all_spec.rb
+++ spec/all_spec.rb
@@ -97,6 +97,27 @@ RSpec.describe EnglishToBraille do
       end
     end
 
+    describe '#match' do
+      it 'can match English letters in message to an array
+          representing itself in Braille' do
+
+          expect(translator.match).to be_a(Array)
+          expect(translator.match[0]).to eq(["0.", "00", ".."])
+          expect(translator.match[11]).to eq(["..", "..", ".."])
+      end
+    end
+
+    describe '#format' do
+      it 'can take each element of an array of arrays
+          and place them into a hash that seperates them 
+          into three keys' do
+
+          expect(translator.format(translator.match)).to be_a(Hash)
+          expect(translator.format(translator.match)[1]).to be_a(Array)
+          expect(translator.format(translator.match)[3]).to be_a(Array)
+      end
+    end
+
     describe '#translate' do
       it 'can change' do
         expect(translator.translate).to eq("0.0.0.0.0....00.0.0.00..0.0.0.0.0....00.0.0.00..0.0.0.0.0....00.0.0.00..0.0.0.0.\n00.00.0..0..00.0000..0..00.00.0..0..00.0000..0..00.00.0..0..00.0000..0..00.00.0.\n....0.0.0....00.0.0.........0.0.0....00.0.0.........0.0.0....00.0.0.........0.0.\n0....00.0.0.00..0.0.0.0.0....00.0.0.00..0.0.0.0.0....00.0.0.00..0.0.0.0.0....00.\n.0..00.0000..0..00.00.0..0..00.0000..0..00.00.0..0..00.0000..0..00.00.0..0..00.0\n0....00.0.0.........0.0.0....00.0.0.........0.0.0....00.0.0.........0.0.0....00.\n0.0.00..0.0.0.0.0....00.0.0.00..\n000..0..00.00.0..0..00.0000..0..\n0.0.........0.0.0....00.0.0.....\n")
